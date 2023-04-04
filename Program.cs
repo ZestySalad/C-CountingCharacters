4c6eb068b3402d7e6559c1fa5ccc8849db1716f3
ZestySalad <121341328+ZestySalad@users.noreply.github.com>
2023-04-03 21:30:26 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Counting 
+{  class Program {
+        static void Main(string[] args)
+{
+            string phrase = "C# is amazing!";
+            
+            int result1 = phrase.Length - phrase.Replace("C", "").Length;
+            
+            int result2 = phrase.Split('C').Length - 1;
+            int result3 = phrase.ToCharArray().Count(c => c == 'C');
+            
+            
+            
+              Console.WriteLine( phrase.Length);
+              Console.ReadLine();
+
+}
+}
+}
